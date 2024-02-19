d5143af074c9d030f0e0ffbaa6bfae9c654a870c
Artiom Khachaturian <Atriom.Khachaturian@digital14.com>
2024-02-19 13:57:37 +0100

    Add comment

--- worker/include/RTC/MediaTranslate/Websocket/WebsocketOptions.hpp
+++ worker/include/RTC/MediaTranslate/Websocket/WebsocketOptions.hpp
@@ -19,6 +19,23 @@ struct WebsocketOptions
     std::string _tlsKeyStore;
     std::string _tlsPrivateKey;
     std::string _tlsPrivateKeyPassword;
+    /* list of possible options:
+     * asio::socket_base::broadcast @n
+     * asio::socket_base::do_not_route @n
+     * asio::socket_base::keep_alive @n
+     * asio::socket_base::linger @n
+     * asio::socket_base::receive_buffer_size @n
+     * asio::socket_base::receive_low_watermark @n
+     * asio::socket_base::reuse_address @n
+     * asio::socket_base::send_buffer_size @n
+     * asio::socket_base::send_low_watermark @n
+     * asio::ip::multicast::join_group @n
+     * asio::ip::multicast::leave_group @n
+     * asio::ip::multicast::enable_loopback @n
+     * asio::ip::multicast::outbound_interface @n
+     * asio::ip::multicast::hops @n
+     * asio::ip::tcp::no_delay
+    */
     std::optional<bool> _tcpNoDelay;
 };
 
