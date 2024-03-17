d8d938b7d2be6c7c7954c4c38b65e3257a514cc7
Artiom Khachaturian <Atriom.Khachaturian@digital14.com>
2024-03-17 13:21:26 +0100

    Adopt lock for SharedLockGuard

--- worker/include/SharedLockGuard.hpp
+++ worker/include/SharedLockGuard.hpp
@@ -1,10 +1,12 @@
 #pragma once
+#include <mutex>
 
 template<class TSharedMutex>
 class SharedLockGuard
 {
 public:
     SharedLockGuard(TSharedMutex& mutex);
+    SharedLockGuard(TSharedMutex& mutex, std::adopt_lock_t);
     ~SharedLockGuard() { _mutex.unlock_shared(); }
 private:
     TSharedMutex& _mutex;
@@ -15,4 +17,10 @@ SharedLockGuard<TSharedMutex>::SharedLockGuard(TSharedMutex& mutex)
     : _mutex(mutex)
 {
     _mutex.lock_shared();
+}
+
+template<class TSharedMutex>
+SharedLockGuard<TSharedMutex>::SharedLockGuard(TSharedMutex& mutex, std::adopt_lock_t)
+    : _mutex(mutex)
+{
 }
